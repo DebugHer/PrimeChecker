735741bea42cc643fe9de12c9e9ed4a24fac8e85
Ayomikun Adegoke <ayomikunadegoke@gmail.com>
2019-08-14 11:32:42 +0100

    Simple iOS prime number checker

--- Is it Prime?/ViewController.swift
+++ Is it Prime?/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var numberEntered: UITextField!
+    @IBOutlet weak var statusPrime: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func checkPrime(_ sender: UIButton) {
+        if let userEnteredString = numberEntered.text{
+            let userEnteredInteger : Int? = Int(userEnteredString)
+            if let sureNumber = userEnteredInteger{
+                var i = 2
+                var isPrime: Bool = true
+                
+                if sureNumber == 1{
+                    isPrime = true
+                }
+                
+                while i < sureNumber{
+                    if sureNumber % i == 0{
+                        isPrime = false
+                    }
+                    i = i + 1
+                }
+                if isPrime{
+                    statusPrime.text = "\(sureNumber) is Prime!"
+                }else{
+                    statusPrime.text = "\(sureNumber) is NOT Prime!"
+                }
+            }else{
+                statusPrime.text = "Please Enter a positive whole number"
+            }
+        }
+        
+    }
+    
 }
 
