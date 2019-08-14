e19b2cebfb948abda02a510eeac4bcf9758c068d
Ayomikun Adegoke <ayomikunadegoke@gmail.com>
2019-08-14 09:34:46 +0100

    Initial Commit

---
+++ Is it Prime?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Is it Prime?
+//
+//  Created by Adegoke Ayomikun on 14/08/2019.
+//  Copyright © 2019 Devmikzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
